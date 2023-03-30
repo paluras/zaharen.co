fee002b57708ad2775e210247ae66bf9a033f0a7
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-03-30 12:55:58 +0300

    phone part 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,21 @@ import { Outlet, Link } from "react-router-dom";
 function Footer(){
     return( <footer>
         <div className="about">
-          <div className="box-red"></div>
+          <div className="box red"></div>
           <Link to="/home"> <p>Home</p></Link>
         </div>
         <div className="contact">
-          <div className="box-green"></div>
+          <div className="box green"></div>
           <p>Contact</p>
         </div>
         
         <div className="news">
-          <div className="box-yellow"></div>
+          <div className="box yellow"></div>
           <p>News</p>
         </div>
         
         <div className="sport">
-          <div className="box-blue"></div>
+          <div className="box blue"></div>
           <p>Sport</p>
         </div>
        </footer>)
