d97c22abe5d5ad8fb7fe31f61cecea04e00e855c
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-04-19 17:08:27 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,29 @@ function App() {
     setSelectedImg(src);
   }
 
+const textMusic = <div className="text-music">
+<div><a href="https://on.soundcloud.com/zCpfC" target="_blank">Piano Sonata No.1 (2022)</a></div>
+<div><a target="_blank" href="https://open.spotify.com/album/4z5ldoG03eOzAsPBvbpCj0?si=oTJCNnzKQK2T6UJ1zU4IvA">Primordial feelings EP (2021)</a></div>
+<div><a href="https://on.soundcloud.com/4Lgkt" target="_blank">Conditions (2021)</a></div>
+<div><a href="https://youtu.be/GlA0zGgIUH8" target="_blank">Inside of you (2020)</a></div>
+<div><a href="https://on.soundcloud.com/ULp64" target="_blank">Provincial (2019)</a></div>
+</div>
+const textMix = <div className="text-music">
+<div>Mixing Engineer - L'elisir d'amore (2023)  - dr. Cristian Mihailescu</div>
+<div>Sound Design - Good, Evil and the Sun (2023) - dr. Yutaro Keino</div>
+<div>Sound Design - Punguista (2022) - dr. Yutaro Keino</div>
+<div>Recording Engineer - Alice Sonia Michael - The Spell (2022)</div>
+<div>Recording Engineer - Mohamad Zatari Trio - Istehlal LP (2022)</div>
+<div>Mixing Engineer - Isabelle (2021)- dr. Cristian Nicolae</div>
+<div>Recording Engineer - Fine It’s Pink - Sailor (2021)</div>
+<div><a target="_blank" href="https://www.youtube.com/watch?v=vhOumBg0gDA&embeds_euri=https%3A%2F%2Feditor.wixapps.net%2F&embeds_origin=https%3A%2F%2Feditor.wixapps.net&feature=emb_logo 
+">Mixing Engineer - Mohamad Zatari Trio - Live Concert (2020)</a></div>
+<div>Recording Engineer - Urma - Love Create Inspire (2020)</div>
+<div><a target="_blank" href="
+https://www.imdb.com/title/tt7624934/?ref_=nm_knf_t_1 
+">Assistent Sound Editor - Zoo (2018) - dr. Antonio Tublen</a></div>
+</div>
+
   const bioText = `Alexandru Zaharencu is a musician, composer, and multimedia artist. His music is distinguished by a dramatic component, a balance of predictability and honesty, and an approach to various genres ranging from jazz to contemporary music, film music, or sound design.
   He was born in Tulcea, Romania, in 1998, and spent 12 years there studying classical piano performance. He continued his education at the National University of Music in Bucharest, where he earned bachelor's degrees in jazz-pop composition under Andrei Tudor and a master's in modern composition under Diana Rotaru.
   As a composer, he has worked on numerous film and theater music projects, as well as advertising and choreographic projects. He additionally exhibited interactive installations such as Primordial feelings, an audio-visual installation based on the conversion of cathode ray tube TVs into oscilloscopes, which was shown at the Diploma 2021 festival, and Catoptro-tono v.1, which is based on catoptrophobia, the fear of mirrors, which was presented at arca.expo 1.3. In addition to his artistic work, he has worked as an audio engineer for music and film sound, as well as a sound designer or foley artist. Since 2021, he has been leading his fusion band Zaharenco, which combines jazz, classical architecture, and rock influences.
@@ -102,6 +125,7 @@ function App() {
           wide range of emotions.
           <a
             style={{ color: "#ff68b4" }}
+            target="_blank"
             href="https://open.spotify.com/album/4z5ldoG03eOzAsPBvbpCj0?si=Tgr-UWa9Rcag4uDjxw-uug"
           >
             {" "}
@@ -209,37 +233,37 @@ function App() {
   const contacts = (
     <div className="contact-text">
       <div>
-        <a href="mailto:zaharencu.alexandru@gmail.com?">
+        <a  href="mailto:zaharencu.alexandru@gmail.com?" target="_blank">
           <img width={"42px"} src="\icons\Mail.png" alt="" />
           Mail
         </a>
       </div>
       <div>
-        <a href="https://www.facebook.com/zaharen.co">
+        <a href="https://www.facebook.com/zaharen.co" target="_blank">
           <img width={"42px"} src="\icons\Facebook.png" alt="" />
           Facebook
         </a>
       </div>
       <div>
-        <a href="https://www.instagram.com/zaharen.co/">
+        <a href="https://www.instagram.com/zaharen.co/" target="_blank">
           <img width={"42px"} src="\icons\Instagram.png" alt="" />
           Instagram
         </a>
       </div>
       <div>
-        <a href="https://www.youtube.com/@zaharenco">
+        <a href="https://www.youtube.com/@zaharenco" target="_blank">
           <img width={"42px"} src="\icons\Youtube.png" alt="" />
           Youtube
         </a>
       </div>
       <div>
-        <a href="https://open.spotify.com/artist/4azDTEsE76hndB8paNOEe8?si=dBL-mht3RRapA-CO_-miEg&fbclid=IwAR2IwJVZyvZxF0uPJiI8uBJBqBTCiKa0-0b0wqabiZUxj5rDimbykAR5saY&nd=1">
+        <a href="https://open.spotify.com/artist/4azDTEsE76hndB8paNOEe8?si=dBL-mht3RRapA-CO_-miEg&fbclid=IwAR2IwJVZyvZxF0uPJiI8uBJBqBTCiKa0-0b0wqabiZUxj5rDimbykAR5saY&nd=1" target="_blank">
           <img width={"42px"} src="\icons\Spotify.png" alt="" />
           Spotify
         </a>
       </div>
       <div>
-        <a href="https://linktr.ee/zaharen.co?utm_source=linktree_profile_share&ltsid=56442ba0-afdf-4bbd-8046-e6c2f7bf8572">
+        <a href="https://linktr.ee/zaharen.co?utm_source=linktree_profile_share&ltsid=56442ba0-afdf-4bbd-8046-e6c2f7bf8572" target="_blank">
           <img width={"42px"} src="\icons\linktree.png" alt="" />
           LinkTree
         </a>
@@ -331,7 +355,7 @@ function App() {
           path="/music"
           element={
             <Blog
-              textBlog={"Music"}
+              textBlog={textMusic}
               tittleBlog={"Music"}
               navItems={navItemsPort}
               crt={crt}
@@ -391,7 +415,7 @@ function App() {
           path="/mixing"
           element={
             <Blog
-              textBlog={"text"}
+              textBlog={textMix}
               tittleBlog={"Mixing"}
               navItems={navItemsPort}
               crt={crt}
