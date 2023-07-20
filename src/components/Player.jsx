39408b03692a0d61170fb91189dc828b1d011af7
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-07-20 09:05:41 +0300

    Fix player and ce voia zahar

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,24 +6,20 @@ function SongPlayer(props) {
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [showProgressBar, setShowProgressBar] = useState(false);
-  const [number, setNumber] = useState()
-  const targetElementRef = useRef(null);
-  const elementXRef = useRef(0); 
+  const progressBarRef = useRef(null);
   function handlePlayPause() {
     const audio = audioRef.current;
 
     if (isPlaying) {
       audio.pause();
-     
     } else {
       audio.play();
       setShowProgressBar(true);
-      
     }
 
     setIsPlaying(!isPlaying);
-   
   }
+ 
 
   function handleTimeUpdate() {
     const audio = audioRef.current;
@@ -34,16 +30,18 @@ function SongPlayer(props) {
 
   function handleProgressBarClick(event) {
     const audio = audioRef.current;
-    const progressBar = document.querySelector("div.progress-bar");
-  
+    const progressBar = progressBarRef.current;
+
     const progressBarRect = progressBar.getBoundingClientRect();
+
     const clickPosition = event.clientX - progressBarRect.left;
+
     const progressBarWidth = progressBar.offsetWidth;
-    
+
     if (progressBarWidth !== 0) {
       const progress = clickPosition / progressBarWidth;
       const time = progress * audio.duration;
-  
+
       if (isFinite(time)) {
         audio.currentTime = time;
         setCurrentTime(time);
@@ -51,30 +49,37 @@ function SongPlayer(props) {
     }
   }
 
- 
-
   const progressBarStyles = {
     width: `${(currentTime / duration) * 100}%`,
-   
   };
 
   return (
     <div className="for-nothing">
       <div className="container-player">
-      <audio key={props.keys} ref={audioRef} src={props.song} onTimeUpdate={handleTimeUpdate} />
-      <img  className="icon"
-        src={isPlaying ? "/icons/stop.svg" : "/icons/play.svg"}
-        alt={isPlaying ? "Stop" : "Play"}
-        onClick={handlePlayPause}
-      />
-      <div onClick={handlePlayPause}  className="test">
-      <div>{props.text}</div>{props.iconLink}
+        <audio
+          key={props.keys}
+          ref={audioRef}
+          src={props.song}
+          onTimeUpdate={handleTimeUpdate}
+        />
+        <img
+          onClick={handlePlayPause}
+          className="icon"
+          src={isPlaying ? "/icons/stop.svg" : "/icons/play.svg"}
+          alt={isPlaying ? "Stop" : "Play"}
+        />
+        <div className="test">
+          <div onClick={handlePlayPause}>{props.text}</div>
+          {props.iconLink}
+        </div>
       </div>
-      
-      </div>
-      
+
       {showProgressBar && (
-        <div className="progress-bar" onClick={handleProgressBarClick}>
+        <div
+          className="progress-bar"
+          onClick={handleProgressBarClick}
+          ref={progressBarRef}
+        >
           <div className="progress-bar-fill" style={progressBarStyles} />
         </div>
       )}
@@ -82,4 +87,4 @@ function SongPlayer(props) {
   );
 }
 
-export default SongPlayer
+export default SongPlayer;
