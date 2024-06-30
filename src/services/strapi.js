58d85fd4c71bc7911a438c8c991209e44ea2c251
paluraandrei1 <paluraandrei1@gmail.com>
2024-06-30 16:20:36 +0300

    ok

--- src/services/strapi.js
+++ src/services/strapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://my-project-n0jf.onrender.com/'; // Replace with your actual API endpoint
+const API_URL = 'https://my-project-n0jf.onrender.com'; // Replace with your actual API endpoint
 
 export const fetchData = async (endpoint) => {
     try {
