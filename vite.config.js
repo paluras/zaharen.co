cab238ab8b0eb17d863adca7790377f32955a6a3
paluraandrei1 <paluraandrei1@gmail.com>
2024-06-30 18:49:45 +0300

    workbrother

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
