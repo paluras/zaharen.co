644b58b0d6703c5e4f3f07ab70bb5b7ef733edcf
paluraandrei1 <paluraandrei1@gmail.com>
2024-07-04 10:58:43 +0300

    move pixel just to linktree

--- src/pages/linktree/Linktree.jsx
+++ src/pages/linktree/Linktree.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { LinktreeButton } from "./components/LinktreeButton";
 import "./linktree.style.css";
+import { Helmet } from "react-helmet";
 
 export default function LinkTree() {
   const [videoSrc, setVideoSrc] = useState("");
@@ -23,6 +24,22 @@ export default function LinkTree() {
 
   return (
     <main className="linktree-main">
+      <Helmet>
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '679345037709625');
+            fbq('track', 'PageView');
+          `}
+        </script>
+      </Helmet>
       <header className="linktree-header">
         <img
           className="linktree-zaharenco-logo"
