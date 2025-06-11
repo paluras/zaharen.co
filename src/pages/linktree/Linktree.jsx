b79f7739ce5e3b04da24aee27d5b120b19f99fd7
paluraandrei1 <paluraandrei1@gmail.com>
2025-06-11 21:39:39 +0300

    implement dynamic fetching of linktree configuration and buttons

--- src/pages/linktree/Linktree.jsx
+++ src/pages/linktree/Linktree.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { LinktreeButton } from "./components/LinktreeButton";
 import "./linktree.style.css";
 import { Helmet } from "react-helmet";
 import { useNavigate } from "react-router-dom";
+import { client } from "../../lib/sanity";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(client);
 
 export default function LinkTree() {
   const navigate = useNavigate();
+  const [config, setConfig] = useState(null);
+  const [buttons, setButtons] = useState([]);
+
+  useEffect(() => {
+    // Query for linktree configuration
+    const configQuery = `*[_type == "linktreeConfig"][0]{
+      title,
+      logo,
+      logoPath,
+      featuredImage,
+      featuredImagePath,
+      featuredImageLink,
+      featuredImageAlt,
+      facebookPixelId
+    }`;
+
+    // Query for active buttons ordered by display order
+    const buttonsQuery = `*[_type == "linktreeButton" && isActive == true] | order(order asc){
+      _id,
+      title,
+      url,
+      platform,
+      icon,
+      iconPath,
+      altText,
+      order,
+      trackingName,
+      openInNewTab
+    }`;
+
+    // Fetch both queries
+    Promise.all([client.fetch(configQuery), client.fetch(buttonsQuery)])
+      .then(([configData, buttonsData]) => {
+        if (configData) {
+          setConfig(configData);
+        }
+        if (buttonsData) {
+          setButtons(buttonsData);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   // Function to track button clicks
   const trackButtonClick = (buttonName) => {
     if (window.fbq) {
@@ -15,30 +64,76 @@ export default function LinkTree() {
     }
   };
 
+  // Get the appropriate icon source
+  const getIconSource = (button) => {
+    // If Sanity image exists, use it
+    if (button.icon) {
+      return builder.image(button.icon).url();
+    }
+
+    // If legacy iconPath exists, use it
+    if (button.iconPath) {
+      return button.iconPath;
+    }
+
+    // Default platform icons (fallback when no icon is provided)
+    const platformIcons = {
+      youtube: "./icons/youtubelogo.png",
+      spotify: "./icons/spotifylogo.png",
+      "apple-music": "./icons/applelogo.png",
+      soundcloud: "./icons/soundcloudlogo.png",
+      "youtube-music": "./icons/youtubemusiclogo.png",
+      bandcamp: "./icons/Logo Bandcamp Edit.png",
+      tidal: "./icons/tidallogo.png",
+    };
+
+    return platformIcons[button.platform] || "";
+  };
+
+  // Get logo source
+  const getLogoSource = () => {
+    if (config?.logo) {
+      return builder.image(config.logo).url();
+    }
+    return config?.logoPath || "./Zaharenco-logo.png";
+  };
+
+  // Get featured image source
+  const getFeaturedImageSource = () => {
+    if (config?.featuredImage) {
+      return builder.image(config.featuredImage).url();
+    }
+    return config?.featuredImagePath || "./Artwork Condition 4 v.5.jpg";
+  };
+
   return (
     <main className="linktree-main">
       <Helmet>
-        <script>
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '438399329170276');
-            fbq('track', 'PageView');
-          `}
-        </script>
+        <title>{config?.title || "Zaharenco - Links"}</title>
+        {config?.facebookPixelId && (
+          <script>
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${config.facebookPixelId}');
+              fbq('track', 'PageView');
+            `}
+          </script>
+        )}
       </Helmet>
+
       <header className="linktree-header">
         <img
           onClick={() => navigate("/")}
           className="linktree-zaharenco-logo"
           aria-hidden
-          src="./Zaharenco-logo.png"
+          src={getLogoSource()}
           alt="logo"
           style={{
             cursor: "pointer",
@@ -47,79 +142,37 @@ export default function LinkTree() {
       </header>
 
       <section>
-        <div className="img-container-linktree">
-          <a
-            target="_blank"
-            href="https://open.spotify.com/track/0mqUU3foODusY7YLK5PJlq?si=g93ZQZC4STimjD57Cyb-DQ&context=spotify%3Aalbum%3A6nWWlyMKA5CzX2LMaXagkU"
-            onClick={() => trackButtonClick("Cover Album")}
-          >
-            <img
-              id="linktree-img"
-              src={"./Artwork Condition 4 v.5.jpg"}
-              alt="Cover Album For Zaharenco"
-            />
-          </a>
-        </div>
-        <LinktreeButton
-          linkProp={"https://youtu.be/xryrrX6E2PM?si=tCr8XsLZtvULaO7x"}
-          imgProp={"./icons/youtubelogo.png"}
-          textProp={""}
-          altText={"YouTube"}
-        />
-        <LinktreeButton
-          linkProp={
-            "https://open.spotify.com/track/0mqUU3foODusY7YLK5PJlq?si=g93ZQZC4STimjD57Cyb-DQ&context=spotify%3Aalbum%3A6nWWlyMKA5CzX2LMaXagkU"
-          }
-          imgProp={"./icons/spotifylogo.png"}
-          textProp={""}
-          altText={"Spotify"}
-        />
-        <LinktreeButton
-          linkProp={
-            "https://open.spotify.com/playlist/7fP4Kd7jjElcPbHKihCmq3?si=7IHYCAQHRguxwPLYhmf04A"
-          }
-          imgProp={""}
-          altText={"New Jazz Romania Playlist"}
-          textProp={"New Jazz Romania Playlist"}
-        />
-        <LinktreeButton
-          linkProp={
-            "https://music.apple.com/au/album/s/1761316442?i=1761316443&app=music&itscg=10002&itsct=mus_1591749000&ct=pTf1e4CmIaPuOWw&at=1010l367Y&ls=1&mttnsubad=song_preadd_Measure"
-          }
-          imgProp={"./icons/applelogo.png"}
-          altText={"Apple Music"}
-          textProp={""}
-        />
-        <LinktreeButton
-          linkProp={"https://on.soundcloud.com/e6hTbUim9KUiwBjg9"}
-          imgProp={"./icons/soundcloudlogo.png"}
-          altText={"SoundCloud"}
-        />
-        <LinktreeButton
-          linkProp={
-            "https://music.youtube.com/watch?v=xryrrX6E2PM&si=qkSJ5K9fvqv_7n0p"
-          }
-          imgProp={"./icons/youtubemusiclogo.png"}
-          altText={"YouTube Music"}
-        />
-        <LinktreeButton
-          linkProp={"https://zaharenco.bandcamp.com"}
-          imgProp={"./icons/Logo Bandcamp Edit.png"}
-          altText={"Tidal"}
-        />
+        {/* Featured Image */}
+        {config && (
+          <div className="img-container-linktree">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={config.featuredImageLink || "#"}
+              onClick={() => trackButtonClick("Cover Album")}
+            >
+              <img
+                id="linktree-img"
+                src={getFeaturedImageSource()}
+                alt={config.featuredImageAlt || "Featured Image"}
+              />
+            </a>
+          </div>
+        )}
 
-        <LinktreeButton
-          linkProp={"https://tidal.com/track/368003631?u"}
-          imgProp={"./icons/tidallogo.png"}
-          altText={"Tidal"}
-        />
-
-        <LinktreeButton
-          linkProp={"./"}
-          imgProp={""}
-          altText={"Website"}
-          textProp={"Website"}
-        />
+        {/* Dynamic Buttons */}
+        {buttons.map((button) => (
+          <LinktreeButton
+            key={button._id}
+            linkProp={button.url}
+            imgProp={getIconSource(button)}
+            textProp={button.title || ""}
+            altText={button.altText}
+            onClick={() =>
+              trackButtonClick(button.trackingName || button.altText)
+            }
+          />
+        ))}
       </section>
     </main>
   );
